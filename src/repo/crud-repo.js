0cbcb9dd45e70d2578733299ce3d41b3328eff31
akashrajakku <akashrajakku263@gmail.com>
2025-04-11 00:21:33 +0530

    cleaned crud-repo by removing all try-catch blocks as error handling is being separately done by services

--- src/repo/crud-repo.js
+++ src/repo/crud-repo.js
@@ -11,51 +11,39 @@ class CrudRepo {
     }
 
     async destroy(data){ //deletion
-        try {
-            const response = await this.model.destroy({
+        const response = await this.model.destroy({
                 where: {
                     id: data
                 }
-            });
-            return response;
-        } catch (error) {
-            logger.error(`An error occurred in Crud-repo : destroy`)
-            throw error;
-        }
+        });
+
+        return response;
+
     }
 
     async get(data){ //The findByPk method obtains only a single entry from the table, using the provided primary key.
-        try {
+        
             const response = await this.model.findByPk(data);
             return response;
-        } catch (error) {
-            logger.error(`An error occurred in Crud-repo : get`)
-            throw error;
-        }
+        
     }
 
     async getAll(){ // It generates a standard SELECT query which will retrieve all entries from the table (unless restricted by something like a where clause, for example).
-        try {
+       
             const response = await this.model.findByAll();
             return response;
-        } catch (error) {
-            logger.error(`An error occurred in Crud-repo : getAll`)
-            throw error;
-        }
+       
     }
 
     async update(id, data){ 
-        try {
-            const response = await this.model.update(data, {
+           
+        const response = await this.model.update(data, {
                 where: {
                     id: id
                 }
             });
             return response;
-        } catch (error) {
-            logger.error(`An error occurred in Crud-repo : update`)
-            throw error;
-        }
+        
     }
 }
 
